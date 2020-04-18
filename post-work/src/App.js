6408c7d1614d77e7476d9a773a23ef277122de76
danforth <danforth@iastate.edu>
2020-04-17 21:03:49 -0500

    more work on Post

--- post-work/src/App.js
+++ post-work/src/App.js
@@ -1,27 +1,45 @@
 import React from 'react';
-import logo from './logo.svg';
+import errorPhoto from './404.jfif';
+import ReactDOM from "react-dom";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class Post extends React.Component 
+{
+  
+  state = {
+    Title: "Title not found",
+    Photo: errorPhoto,
+    Likes: 0
+  }
+  dislike = () => {
+    this.setState({ Likes: this.state.Likes - 1 });
+  }
+  like = () => {
+    this.setState({ Likes: this.state.Likes + 1 });
+  }
+  comment = () => {
+
+  }
+  render()  
+  {
+    return (
+      <div className="Post">
+        <header className="Post-header">
+          <p>
+          {this.state.Title}
+          </p>
+          <img src= {this.state.Photo} className="Post-logo" alt="Photo" />
+          
+          <p> <button onClick={this.like}>Upvote</button>  {this.state.Likes}  <button onClick={this.dislike}>Downvote</button></p>
+           <p><input 
+          
+          className = "commentBox"
+          placeholder = "Make your comment here!"
+          value={this.state.comment}>
+
+          </input>   <button onClick={this.comment}>Comment</button></p>
+        </header>
+      </div>
+    );
+  }
 }
-
-export default App;
-
